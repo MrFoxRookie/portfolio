ec250808959f45e8b58a10847046934278c50f0e
MrFoxRookie <Zahidtejedaamaro@gmail.com>
2026-05-30 14:45:01 -0600

    Base creada del proyecto

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css"
+import Header from "../Footer/Footer"
+import Main from "../Main/Main"
+import Footer from "../Footer/Footer"
+
+function App() {
+  return (
+    <>
+  <Header/>
+  <Main/>
+  <Footer/>
+    </>
+  )
+}
+
+export default App
