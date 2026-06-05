6c5df08e3ab71d3e69643620bf4799746ba97154
MrFoxRookie <Zahidtejedaamaro@gmail.com>
2026-06-04 20:28:57 -0600

    Ajuste de de archivos para Github Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/portfolio/",
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/portfolio/" : "/",
+  };
 });
